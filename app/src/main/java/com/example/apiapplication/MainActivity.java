e3cc551aee48b7558bf04e85987df1aa2c7cafc7
Joseph Hugo <Josephjhugo@example.com>
2021-10-28 11:31:01 +0800

    first commit

---
+++ app/src/main/java/com/example/apiapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.apiapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText username, password;
+    public MaterialButton loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usernameText = username.getText().toString();
+                String passwordText = password.getText().toString();
+
+                Toast.makeText(MainActivity.this, usernameText + " = " + passwordText, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
